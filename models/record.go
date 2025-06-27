d9a6340cf6563856f741e1cf2419e8aaca1e8f30
Hailey <me@haileyok.com>
2025-06-27 14:31:58 -0700

    fix: missing ch tag

--- models/record.go
+++ models/record.go
@@ -3,11 +3,11 @@ package models
 import "time"
 
 type Record struct {
-	Did        string `ch:"did"`
-	Rkey       string `ch:"rkey"`
-	Collection string `ch:"collection"`
-	Cid        string `ch:"cid"`
-	Seq        string `ch:"seq"`
-	Raw        []byte `ch:"raw"`
-	CreatedAt  time.Time
+	Did        string    `ch:"did"`
+	Rkey       string    `ch:"rkey"`
+	Collection string    `ch:"collection"`
+	Cid        string    `ch:"cid"`
+	Seq        string    `ch:"seq"`
+	Raw        []byte    `ch:"raw"`
+	CreatedAt  time.Time `ch:"created_at"`
 }
